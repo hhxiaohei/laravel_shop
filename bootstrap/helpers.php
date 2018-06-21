89a2113c9faf641453013bbd22c4983681ea047a
MIke Zhang <mike@baidudev.cn>
2018-06-21 23:06:02 +0800

    Update

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -2,3 +2,6 @@
 function test_helper(){
     return 'OK';
 }
+function route_class(){
+    return str_replace('.','-',Route::currentRouteName());
+}
---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            测试
+        </p>
+
+        <p class="pull-right"><a href="mailto:root@leo108.com">联系我们</a></p>
+    </div>
+</footer>
